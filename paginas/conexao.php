06da6e48593c37c0c54afdb63910e53d6f7e1760
cassio-mendes <kassiogithub@gmail.com>
2024-09-13 15:42:10 -0300

    Reorganizando o index
    Coloquei o index.html na pasta raiz

--- paginas/conexao.php
+++ paginas/conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $connection = new mysqli("localhost:3307", "nibble", "-JCk]lNpJp", "nibble_db"); //No servidor
+    $connection = new mysqli("52.233.90.226", "nibble", "-JCk]lNpJp", "nibble_db"); //No servidor
     //$connection = new mysqli("localhost:3307", "nibble", "-JCk]lNpJp", "Nibble"); //Local
     
     if ($connection->connect_error) {
