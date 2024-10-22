e8b1b1c89c2358871ed3c0430fee90851506de52
cassio-mendes <kassiogithub@gmail.com>
2024-10-22 16:32:20 -0300

    aaa

--- paginas/validar-login.php
+++ paginas/validar-login.php
@@ -3,14 +3,15 @@
     $email = $_POST["email"];
     $senha = $_POST["password"];
 
-    $sql = "SELECT email, senha, admin FROM usuario where email = '$email' and senha = '$senha'";
+    $sql = "SELECT email, senha, adm FROM usuario where email = '$email' and senha = '$senha'";
     $statement = $pdo->prepare($sql);
     $statement->execute();
     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
     if(count($result) > 0) {
-        if($result["admin"] === 1){
+        if($result["adm"] === 1){
             header("mainPageADM.html");
+            echo "foi";
         } else { //Não é um adm
             header("mainPage.html");
         }           
