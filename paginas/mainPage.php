c120018a5d095d78cc72c9c5a7f5f8f242b09037
cassio-mendes <kassiogithub@gmail.com>
2024-10-24 15:49:05 -0300

    s

--- paginas/mainPage.php
+++ paginas/mainPage.php
@@ -28,6 +28,7 @@
             <a href="cardapioCliente.html" class="btn">Ver Cardápio</a>
             <a href="gerirCompra.html" class="btn">Minhas Compras</a>
             <a href="pratoDoDia.html" class="btn">Prato do Dia</a>
+            <a href="../index.html" class="btn">Sair</a>
         </div>
     </div>
 </body>
