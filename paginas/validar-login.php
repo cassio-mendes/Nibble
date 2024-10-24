07c873a8fe37be90856afa5bd23bd8a0496ca6c6
cassio-mendes <kassiogithub@gmail.com>
2024-10-24 14:31:04 -0300

    e

--- paginas/validar-login.php
+++ paginas/validar-login.php
@@ -7,15 +7,12 @@
     $statement = $pdo->prepare($sql);
     $statement->execute();
     $result = $statement->fetch(PDO::FETCH_ASSOC);
-    echo "adm:" . $result["adm"] . "\n";
-    echo "email:" . $result["email"] . "\n";
-    echo "senha:" . $result["senha"] . "\n";
-
+    
     if(count($result) > 0) { //Se há pelo menos um resultado
         if($result["adm"] === 1){ //É um adm
-            //header("Location: nibble/mainPageADM.html");
+            header("Location: nibble/mainPageADM.html");
         } else { //Não é um adm
-            //header("Location: /nibble/paginas/mainPage.html");
+            header("Location: /nibble/paginas/mainPage.html");
         }           
     } else {
         ?>
