ce5b1f6b270a0e44b4f38aafea13e83b89182a7e
cassio-mendes <kassiogithub@gmail.com>
2024-10-24 16:22:29 -0300

    l

--- paginas/login.php
+++ paginas/login.php
@@ -1,9 +1,13 @@
 <?php
+    session_start();
+
     if(isset($_SESSION['adm'])) {
         if($_SESSION['adm'] === 1) {
             header("Location: /nibble/paginas/mainPageADM.php");
+            exit();
         } else {
             header("Location: /nibble/paginas/mainPage.php");
+            exit();
         }
     }
 ?>
