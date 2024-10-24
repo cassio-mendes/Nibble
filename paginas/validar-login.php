523b3a56fc38c048c632f88d0d21e57b69ca7c4b
cassio-mendes <kassiogithub@gmail.com>
2024-10-24 14:19:52 -0300

    y

--- paginas/validar-login.php
+++ paginas/validar-login.php
@@ -13,9 +13,9 @@
 
     if(count($result) > 0) { //Se há pelo menos um resultado
         if($result["adm"] === 1){ //É um adm
-            header("Location: nibble/mainPageADM.html");
+            //header("Location: nibble/mainPageADM.html");
         } else { //Não é um adm
-            header("Location: /nibble/paginas/mainPage.html");
+            //header("Location: /nibble/paginas/mainPage.html");
         }           
     } else {
         ?>
