a97f66556b28e58e2f312beafbd1476b4d7ac011
cassio-mendes <kassiogithub@gmail.com>
2024-11-07 13:44:26 -0300

    Fazendo ajustes

--- paginas/verifica-email.php
+++ paginas/verifica-email.php
@@ -8,8 +8,8 @@
     $statement->execute();
     $result = $statement->fetch();
 
-    if(count($result) > 0) { //O Usuário está logado
-        header("Location: /nibble/paginas/senhaRecuperada.html");
+    if(count($result) > 0) { //O Usuário possui um email no sistema
+        header("Location: /nibble/paginas/recuperarSenha.html");
     } else {
         ?>
             <script>alert('Este email não está cadastrado')</script>
