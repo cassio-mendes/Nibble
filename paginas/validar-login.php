c4c1f55e50914df4504c4e830a3b5e85d702410a
cassio-mendes <kassiogithub@gmail.com>
2024-11-07 15:50:01 -0300

    Consertando o login
    O login começou a dar erro após a implementação da criptografia de senha. Essas alterações são para corrigir o problema.

--- paginas/validar-login.php
+++ paginas/validar-login.php
@@ -3,12 +3,12 @@
     $email = $_POST["email"];
     $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); //Criptografando a senha
 
-    $sql = "SELECT * FROM usuario where email = '$email' and senha = '$senha'";
+    $sql = "SELECT senha FROM usuario WHERE email = $email;";
     $statement = $pdo->prepare($sql);
     $statement->execute();
     $result = $statement->fetch(PDO::FETCH_ASSOC);
     
-    if(count($result) > 0) { //Se há pelo menos um resultado, o login tá certo
+    if(count($result) > 0 && password_verify($_POST["senha"], $senha)) { //Se há pelo menos um resultado e a senha é correta
         session_start();
         $_SESSION['nome'] = $result['nome'];
         $_SESSION['email'] = $result['email'];
