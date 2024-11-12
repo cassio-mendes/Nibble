60a0ba4c49711592c37e36fe278ec4dcdf46e09a
cassio-mendes <kassiogithub@gmail.com>
2024-11-12 14:55:47 -0300

    .

--- paginas/validar-cadastro.php
+++ paginas/validar-cadastro.php
@@ -4,7 +4,7 @@
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); //Criptografando a senha
-    $telefone = $_POST["telefone"];
+    $telefone = $_POST["tel"];
 
     $sql = "INSERT INTO usuario (nome, email, senha, telefone, adm) VALUES (:nome, :email, :senha, :telefone, false);";
     $statement = $pdo->prepare($sql);
