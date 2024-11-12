e186fc0bf664b88a0a825cd78b89f15518a4c06f
cassio-mendes <kassiogithub@gmail.com>
2024-11-12 14:40:40 -0300

    .

--- paginas/validar-login.php
+++ paginas/validar-login.php
@@ -1,38 +1,44 @@
 <?php
     include_once "conexao.php";
-    $email = $_POST["email"];
-    $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); //Criptografando a senha
-    //echo('Senha: ' . $_POST['senha'] . ' Criptografada: ' . $senha);
-
-    $sql = "SELECT senha FROM usuario WHERE email = '" . $email . "';";
-    $statement = $pdo->prepare($sql);
-    $statement->execute();
-    $result = $statement->fetch(PDO::FETCH_ASSOC);
     
-    if(!$result) {
-        echo('A sql não funcionou');
-        echo($result['senha']);
-    }
+    try {
+        $email = $_POST["email"];
+        $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); //Criptografando a senha
+        echo('Email: ' . $email);
 
-    if(count($result) > 0 && password_verify($senha, $result['senha'])) { //Se há pelo menos um resultado e a senha é correta
-        session_start();
-        $_SESSION['nome'] = $result['nome'];
-        $_SESSION['email'] = $result['email'];
-        $_SESSION['senha'] = $result['senha'];
-        $_SESSION['telefone'] = $result['telefone'];
-        $_SESSION['adm'] = $result['adm'];
+        $sql = "SELECT senha FROM usuario WHERE email = ':email';";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        
+        if(!$result) {
+            echo('A sql não funcionou');
+            echo($result['senha']);
+        }
 
-        if($_SESSION['adm'] === 1){ //É um adm
-            header("Location: /nibble/paginas/mainPageADM.php");
-        } else { //Não é um adm
-            header("Location: /nibble/paginas/mainPage.php");
-        }           
-    } else {
-        ?>
-        <script>
-            alert("Email ou senha incorretos :(") //Erro de login
-            history.back()
-        </script>
-        <?php
+        if(count($result) > 0 && password_verify($senha, $result['senha'])) { //Se há pelo menos um resultado e a senha é correta
+            session_start();
+            $_SESSION['nome'] = $result['nome'];
+            $_SESSION['email'] = $result['email'];
+            $_SESSION['senha'] = $result['senha'];
+            $_SESSION['telefone'] = $result['telefone'];
+            $_SESSION['adm'] = $result['adm'];
+
+            if($_SESSION['adm'] === 1){ //É um adm
+                header("Location: /nibble/paginas/mainPageADM.php");
+            } else { //Não é um adm
+                header("Location: /nibble/paginas/mainPage.php");
+            }           
+        } else {
+            ?>
+            <script>
+                alert("Email ou senha incorretos :(") //Erro de login
+                history.back()
+            </script>
+            <?php
+        }
+    } catch(Exception $e) {
+        echo('Exceção: ' . $e->getMessage());
     }
 ?>
