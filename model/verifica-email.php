ccac25e1dbc3ace0708f49a9d1659264f5d5710d
cassio-mendes <kassiogithub@gmail.com>
2024-11-28 16:00:26 -0300

    .

--- model/verifica-email.php
+++ model/verifica-email.php
@@ -4,19 +4,17 @@
     $email = $_POST['email'];
 
     $sql = "SELECT email FROM usuario WHERE email = '$email';";
+    echo $sql;
     $statement = $pdo->prepare($sql);
     $statement->execute();
     $result = $statement->fetch();
-    ?>
-        <script>alert($result);</script>
-    <?php
-
+    
     if(count($result) > 0) { //O Usuário possui um email no sistema
-        header("Location: /nibble/paginas/recuperarSenha.html");
+        //header("Location: /nibble/paginas/recuperarSenha.html");
     } else {
         ?>
             <script>alert('Este email não está cadastrado')</script>
         <?php
-        header("Location: '/nibble/paginas/login.php'");
+        //header("Location: '/nibble/paginas/login.php'");
     }
 ?>
