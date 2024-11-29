166cdf871c74708355dc6364b75dcc5085e41616
cassio-mendes <kassiogithub@gmail.com>
2024-11-29 07:42:53 -0300

    .

--- model/verifica-email.php
+++ model/verifica-email.php
@@ -1,28 +1,31 @@
 <?php
     include_once "conexao.php";
+    try {
+        $email = $_POST['email'];
 
-    $email = $_POST['email'];
-
-    $sql = "SELECT email FROM usuario WHERE email = :email;";
-    echo $sql;
-    $statement = $pdo->prepare($sql);
-    $statement->bindParam(':email', $email);
-    $statement->execute();
-    $result = $statement->fetch();
-    ?>
-        <script>console.log("executou a query")</script>
-    <?php
-    
-    if($result) { //O Usuário possui um email no sistema
+        $sql = "SELECT email FROM usuario WHERE email = :email;";
+        echo $sql;
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+        $result = $statement->fetch();
         ?>
-            <script>console.log("Email existe")</script>
+            <script>console.log("executou a query")</script>
         <?php
-        //header("Location: '../paginas/recuperarSenha.html'");
-    } else {
-        ?>
-            <script>console.log("Email não existe")</script>
-        <?php
-        echo 'else';
-        //header("Location: '../paginas/login.php'");
+        
+        if($result) { //O Usuário possui um email no sistema
+            ?>
+                <script>console.log("Email existe")</script>
+            <?php
+            //header("Location: '../paginas/recuperarSenha.html'");
+        } else {
+            ?>
+                <script>console.log("Email não existe")</script>
+            <?php
+            echo 'else';
+            //header("Location: '../paginas/login.php'");
+        }
+    } catch(Error $erro) {
+        echo "ERRO " . $erro->getMessage();
     }
 ?>
