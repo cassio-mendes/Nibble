bebeab3d398c427c7edf8517404264a4903b3d5f
cassio-mendes <kassiogithub@gmail.com>
2025-01-22 21:30:53 -0300

    Tentando depurar o código

--- model/colocar-carrinho.php
+++ model/colocar-carrinho.php
@@ -1,9 +1,17 @@
 <?php
     include_once "../model/conexao.php";
 
-    $sql = "INSERT INTO listaprodutos (idUser, idProduto) VALUES (:idUser, :idProduto)";
-    $statement = $pdo->prepare($sql);
-    $statement->bindParam(":idUser", $_POST['idUser']);
-    $statement->bindParam(":idProduto", $_POST['idProduto']);
-    $statement->execute();
+    echo $_POST['idUser'];
+    echo $_POST['idProduto'];
+
+    try {
+        $sql = "INSERT INTO listaprodutos (idUser, idProduto) VALUES (:idUser, :idProduto)";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(":idUser", $_POST['idUser']);
+        $statement->bindParam(":idProduto", $_POST['idProduto']);
+        $statement->execute();
+    } catch(Exception $ex) {
+        echo "ERRO: " . $ex->getMessage();
+    }
+    
 ?>
